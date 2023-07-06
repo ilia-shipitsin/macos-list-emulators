3ecc9185bd7a068d9fa7972fea648c347ff5a68e
Ilia Shipitsin <ilia-shipitsin@github.com>
2023-07-06 20:37:16 +0200

    test

--- list-sim.sh
+++ list-sim.sh
@@ -5,6 +5,6 @@ mkdir ~/sims
 cd /Applications
 xcodes=`find . -type d -name "Xcode*" -maxdepth 1 -print | awk -F '/' '{printf("%s\n", $2);}'`
 for xcode in $xcodes; do
-  echo $xcode | awk '{printf("/Applications/%s/Contents/Developer/usr/bin/simctl list -j devices\n", $2);}' | sh > ~/sims/$xcode
+  echo $xcode | awk '{printf("/Applications/%s/Contents/Developer/usr/bin/simctl list -j devices\n", $1);}' | sh > ~/sims/$xcode
 done
 
